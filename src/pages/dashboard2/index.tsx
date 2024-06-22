c89fd7d06d5b7fac4cc57830622766e3e318655c
徐凌峰 <xulingfeng@yangxuan.net>
2024-06-22 13:37:10 +0800

    feat

--- src/pages/dashboard2/index.tsx
+++ src/pages/dashboard2/index.tsx
@@ -57,14 +57,13 @@ function DashboardTow() {
   const [rank, setRank] = useState(0);
   const [yourEntries, setYourEntries] = useState(0);
   const [yourWinChance, setYourWinChance] = useState(0);
-  const [spinning, setSpinning] = useState(false);
   const getRound = async () => {
-    const roundNumber = await getLatestRoundNumber();
-    setRoundNumber(roundNumber);
-    setRoundList(Array.from({ length: roundNumber }).map((_, i) => i + 1));
+    const newroundNumber = await getLatestRoundNumber();
+    setRoundNumber(newroundNumber);
+    if (newroundNumber === roundList.length) return;
+    setRoundList(Array.from({ length: newroundNumber }).map((_, i) => i + 1));
   };
   const getData = async () => {
-    setSpinning(true);
     getRoundAllData(roundNumber)
       .then((res) => {
         const { addresses, roundData, taxes, eths } = res;
@@ -83,201 +82,200 @@ function DashboardTow() {
         const index = addresses.indexOf(address);
         setYourEntries(taxes[index] || 0);
         setRank(index + 1);
-        setSpinning(false);
       })
-      .catch(() => {
-        setSpinning(false);
-      });
+      .catch(() => {});
   };
+  // 监听区块链地址变化
   useEffect(() => {
     getRound();
+    const interval = setInterval(() => {
+      getRound();
+    }, 10000);
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
   useEffect(() => {
     getData();
-  }, [roundNumber]);
+  }, [roundNumber, address, roundList.length]);
 
   return (
-    <Spin size="large" spinning={spinning}>
-      <DashboardTowBox>
-        <DashboardTowLeft>
-          <Card
-            style={{
-              height: "100%",
-            }}
-          >
-            <DashboardTowLeftTop>
-              <DashboardTowTitle>{players.length} Players</DashboardTowTitle>
-              {/* <Watching>132 Watching</Watching> */}
-            </DashboardTowLeftTop>
-            <DashboardTowLabelBox>
-              {players.map((player, index) => {
-                return (
-                  <DashboardTowLabelItem key={index} color={player.color}>
-                    {/* <DashboardTowLabelItemHeader src={player.header} /> */}
-                    <DashboardTowLabelItemContent>
-                      <DashboardTowLabelItemData>
-                        <Tooltip content={player.name}>
-                          <DashboardTowLabelItemName>
-                            {formatAddress(player.name)}
-                          </DashboardTowLabelItemName>
-                        </Tooltip>
-                        <DashboardTowLabelItemDataPercent>
-                          {((player.taxe / narkTaxEthAnout) * 100).toFixed(2)}%
-                        </DashboardTowLabelItemDataPercent>
-                      </DashboardTowLabelItemData>
-                      <DashboardTowLabelItemData>
-                        <DashboardTowLabelItemPts>
-                          {(player.taxe * 1).toFixed(2)} NONO
-                        </DashboardTowLabelItemPts>
-                        <DashboardTowLabelItemDataCoin>
-                          {player.ethNum} ETH
-                        </DashboardTowLabelItemDataCoin>
-                      </DashboardTowLabelItemData>
-                    </DashboardTowLabelItemContent>
-                  </DashboardTowLabelItem>
-                );
-              })}
-            </DashboardTowLabelBox>
-          </Card>
-        </DashboardTowLeft>
-        <DashboardTowContent>
-          <Card
-            style={{
-              height: "730px",
-            }}
-          >
-            <DashboardTowTitle>Current Round</DashboardTowTitle>
-            <DashboardEchart>
-              <ReactECharts
-                style={{
-                  height: "600px",
-                  width: "100%",
-                }}
-                option={{
-                  series: [
-                    {
-                      name: "Access From",
-                      type: "pie",
-                      radius: ["50%", "90%"],
-                      avoidLabelOverlap: false,
-                      label: {
-                        show: true,
-                        formatter: "{d}%",
-                      },
-                      labelLine: {
-                        show: true,
-                        normal: {},
-                      },
-                      data:
-                        players.map((player) => {
-                          return {
-                            value: player.taxe,
-                            name: player.name,
-                            itemStyle: {
-                              color: player.color,
-                            },
-                          };
-                        }) || [],
+    <DashboardTowBox>
+      <DashboardTowLeft>
+        <Card
+          style={{
+            height: "100%",
+          }}
+        >
+          <DashboardTowLeftTop>
+            <DashboardTowTitle>{players.length} Players</DashboardTowTitle>
+            {/* <Watching>132 Watching</Watching> */}
+          </DashboardTowLeftTop>
+          <DashboardTowLabelBox>
+            {players.map((player, index) => {
+              return (
+                <DashboardTowLabelItem key={index} color={player.color}>
+                  {/* <DashboardTowLabelItemHeader src={player.header} /> */}
+                  <DashboardTowLabelItemContent>
+                    <DashboardTowLabelItemData>
+                      <Tooltip content={player.name}>
+                        <DashboardTowLabelItemName>
+                          {formatAddress(player.name)}
+                        </DashboardTowLabelItemName>
+                      </Tooltip>
+                      <DashboardTowLabelItemDataPercent>
+                        {((player.taxe / narkTaxEthAnout) * 100).toFixed(2)}%
+                      </DashboardTowLabelItemDataPercent>
+                    </DashboardTowLabelItemData>
+                    <DashboardTowLabelItemData>
+                      <DashboardTowLabelItemPts>
+                        {(player.taxe * 1).toFixed(2)} NONO
+                      </DashboardTowLabelItemPts>
+                      <DashboardTowLabelItemDataCoin>
+                        {player.ethNum} ETH
+                      </DashboardTowLabelItemDataCoin>
+                    </DashboardTowLabelItemData>
+                  </DashboardTowLabelItemContent>
+                </DashboardTowLabelItem>
+              );
+            })}
+          </DashboardTowLabelBox>
+        </Card>
+      </DashboardTowLeft>
+      <DashboardTowContent>
+        <Card
+          style={{
+            height: "730px",
+          }}
+        >
+          <DashboardTowTitle>Current Round</DashboardTowTitle>
+          <DashboardEchart>
+            <ReactECharts
+              style={{
+                height: "600px",
+                width: "100%",
+              }}
+              option={{
+                series: [
+                  {
+                    name: "Access From",
+                    type: "pie",
+                    radius: ["50%", "90%"],
+                    avoidLabelOverlap: false,
+                    label: {
+                      show: true,
+                      formatter: "{d}%",
                     },
-                  ],
-                }}
+                    labelLine: {
+                      show: true,
+                      normal: {},
+                    },
+                    data:
+                      players.map((player) => {
+                        return {
+                          value: player.taxe,
+                          name: player.name,
+                          itemStyle: {
+                            color: player.color,
+                          },
+                        };
+                      }) || [],
+                  },
+                ],
+              }}
+            />
+            <DashboardEchartText>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/6/6f/Ethereum-icon-purple.svg"
+                alt=""
               />
-              <DashboardEchartText>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/6/6f/Ethereum-icon-purple.svg"
-                  alt=""
-                />
-                <span>
-                  {players.reduce((prev, curr) => {
-                    return prev + Number(curr.ethNum);
-                  }, 0)}{" "}
-                  ETH
-                </span>
-              </DashboardEchartText>
-            </DashboardEchart>
-          </Card>
-          <Card
-            style={{
-              height: "200px",
-            }}
-          >
-            <DashboardTowTitle>Round Content</DashboardTowTitle>
-            <CoinBox>
-              {
-                roundList.map((item) => {
-                  return (
-                    <CoinCard
-                      key={item}
-                      onClick={() => {
-                        setRoundNumber(item);
-                      }}
-                    >
-                      Round {item}
-                    </CoinCard>
-                  );
-                })
-                // 当前轮次为最后一轮 创建轮次的数组
-              }
-            </CoinBox>
-          </Card>
-        </DashboardTowContent>
-        <DashboardTowRight>
-          <Card
-            style={{
-              height: "200px",
-            }}
-          >
-            <DashboardTowTitle>Round {roundNumber}</DashboardTowTitle>
-            <DashboardInfo>
-              <DashboardInfoItem>
-                <DashboardInfoItemData>
-                  <DashboardInfoItemTop>
-                    {narkTaxEthAnout?.toFixed(2)}
-                  </DashboardInfoItemTop>
-                  <DashboardInfoItemText>Prize Pool</DashboardInfoItemText>
-                </DashboardInfoItemData>
-                <DashboardInfoItemData>
-                  <DashboardInfoItemTop>
-                    {rank}/{players.length}
-                  </DashboardInfoItemTop>
-                  <DashboardInfoItemText>Players</DashboardInfoItemText>
-                </DashboardInfoItemData>
-              </DashboardInfoItem>
-              <DashboardInfoItem>
-                <DashboardInfoItemData>
-                  <DashboardInfoItemTop>{yourEntries}</DashboardInfoItemTop>
-                  <DashboardInfoItemText>Your Entries</DashboardInfoItemText>
-                </DashboardInfoItemData>
-                <DashboardInfoItemData>
-                  <DashboardInfoItemTop>{yourWinChance}</DashboardInfoItemTop>
-                  <DashboardInfoItemText>Your Win Chance</DashboardInfoItemText>
-                </DashboardInfoItemData>
-              </DashboardInfoItem>
-            </DashboardInfo>
-            <Line />
-            <DashboardInfo>
-              <DashboardInfoItem>
-                <DashboardInfoItemData>
-                  <DashboardInfoItemTop>-</DashboardInfoItemTop>
-                  <DashboardInfoItemText>
-                    Your Future Entries
-                  </DashboardInfoItemText>
-                </DashboardInfoItemData>
-                <DashboardInfoItemData>
-                  <DashboardInfoItemTop>-</DashboardInfoItemTop>
-                  <DashboardInfoItemText>Total (0 Avg)</DashboardInfoItemText>
-                </DashboardInfoItemData>
-              </DashboardInfoItem>
-            </DashboardInfo>
-          </Card>
-          <Card
-            style={{
-              height: "calc(100% - 290px)",
-            }}
-          ></Card>
-        </DashboardTowRight>
-      </DashboardTowBox>
-    </Spin>
+              <span>
+                {players.reduce((prev, curr) => {
+                  return prev + Number(curr.ethNum);
+                }, 0)}{" "}
+                ETH
+              </span>
+            </DashboardEchartText>
+          </DashboardEchart>
+        </Card>
+        <Card
+          style={{
+            height: "200px",
+          }}
+        >
+          <DashboardTowTitle>Round Content</DashboardTowTitle>
+          <CoinBox>
+            {roundList.map((item) => {
+              return (
+                <CoinCard
+                  key={item}
+                  onClick={() => {
+                    setRoundNumber(item);
+                  }}
+                >
+                  Round {item}
+                </CoinCard>
+              );
+            })}
+          </CoinBox>
+        </Card>
+      </DashboardTowContent>
+      <DashboardTowRight>
+        <Card
+          style={{
+            height: "200px",
+          }}
+        >
+          <DashboardTowTitle>Round {roundNumber}</DashboardTowTitle>
+          <DashboardInfo>
+            <DashboardInfoItem>
+              <DashboardInfoItemData>
+                <DashboardInfoItemTop>
+                  {narkTaxEthAnout?.toFixed(2)}
+                </DashboardInfoItemTop>
+                <DashboardInfoItemText>Prize Pool</DashboardInfoItemText>
+              </DashboardInfoItemData>
+              <DashboardInfoItemData>
+                <DashboardInfoItemTop>
+                  {rank}/{players.length}
+                </DashboardInfoItemTop>
+                <DashboardInfoItemText>Players</DashboardInfoItemText>
+              </DashboardInfoItemData>
+            </DashboardInfoItem>
+            <DashboardInfoItem>
+              <DashboardInfoItemData>
+                <DashboardInfoItemTop>{yourEntries}</DashboardInfoItemTop>
+                <DashboardInfoItemText>Your Entries</DashboardInfoItemText>
+              </DashboardInfoItemData>
+              <DashboardInfoItemData>
+                <DashboardInfoItemTop>{yourWinChance}</DashboardInfoItemTop>
+                <DashboardInfoItemText>Your Win Chance</DashboardInfoItemText>
+              </DashboardInfoItemData>
+            </DashboardInfoItem>
+          </DashboardInfo>
+          <Line />
+          <DashboardInfo>
+            <DashboardInfoItem>
+              <DashboardInfoItemData>
+                <DashboardInfoItemTop>-</DashboardInfoItemTop>
+                <DashboardInfoItemText>
+                  Your Future Entries
+                </DashboardInfoItemText>
+              </DashboardInfoItemData>
+              <DashboardInfoItemData>
+                <DashboardInfoItemTop>-</DashboardInfoItemTop>
+                <DashboardInfoItemText>Total (0 Avg)</DashboardInfoItemText>
+              </DashboardInfoItemData>
+            </DashboardInfoItem>
+          </DashboardInfo>
+        </Card>
+        <Card
+          style={{
+            height: "calc(100% - 290px)",
+          }}
+        ></Card>
+      </DashboardTowRight>
+    </DashboardTowBox>
   );
 }
 
